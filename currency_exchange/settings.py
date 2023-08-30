181d17a340cfa73fbc7bc68e51cfeb4cd04579b1
garusocruz <garusotec@gmail.com>
2023-08-30 16:34:20 -0300

    chore: setup Django Rest Framework

--- currency_exchange/settings.py
+++ currency_exchange/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework", 
+    "exchange_rates.infra",
     "exchange_rates"
 ]
 
